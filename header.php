5353cb9a38fd1838ba2826cfb4ccfaf2d44a3ffd
Mai Kosoba <mmm.tate@gmail.com>
2018-01-06 23:34:21 +0900

    Change menu

--- header.php
+++ header.php
@@ -33,23 +33,6 @@
 	        <li class="nav-link"><a href="javascript:void(0)">Marcofabrika</a></li>
 	        <li class="nav-link"><a href="javascript:void(0)">Blog</a></li>
 	        <li class="nav-link"><a href="javascript:void(0)">お問い合わせ</a></li>
-	        <li id="js-navigation-more" class="nav-link more"><a href="javascript:void(0)">More</a>
-	          <ul class="submenu">
-	            <li><a href="javascript:void(0)">Submenu Item</a></li>
-	            <li><a href="javascript:void(0)">Another Item</a></li>
-	            <li class="more"><a href="javascript:void(0)">Item with submenu</a>
-	              <ul class="submenu">
-	                <li><a href="javascript:void(0)">Sub-submenu Item</a></li>
-	                <li><a href="javascript:void(0)">Another Item</a></li>
-	              </ul>
-	            </li>
-	            <li class="more"><a href="javascript:void(0)">Another submenu</a>
-	              <ul class="submenu">
-	                <li><a href="javascript:void(0)">Sub-submenu</a></li>
-	                <li><a href="javascript:void(0)">An Item</a></li>
-	              </ul>
-	            </li>
-	          </ul>
 	        </li>
 	      </ul>
 	    </nav>
